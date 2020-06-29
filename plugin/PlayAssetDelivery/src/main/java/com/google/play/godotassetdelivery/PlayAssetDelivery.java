f68fa3ec8e4c6d7adcb65c6034e88dfe491f5251
John Ma <mayilinj@gmail.com>
2020-06-29 15:43:56 -0500

    initialized Android plugin repo, created empty PlayAssetDelivery Java class

---
+++ plugin/PlayAssetDelivery/src/main/java/com/google/play/godotassetdelivery/PlayAssetDelivery.java
@@ -0,0 +1,18 @@
+package com.google.play.godotassetdelivery;
+
+import androidx.annotation.NonNull;
+
+import org.godotengine.godot.Godot;
+import org.godotengine.godot.plugin.GodotPlugin;
+
+public class PlayAssetDelivery extends GodotPlugin {
+    public PlayAssetDelivery(Godot godot) {
+        super(godot);
+    }
+
+    @NonNull
+    @Override
+    public String getPluginName() {
+        return null;
+    }
+}
