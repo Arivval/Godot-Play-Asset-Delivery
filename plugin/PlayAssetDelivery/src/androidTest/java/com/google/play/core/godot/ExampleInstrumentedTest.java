00bca578b103807fbe88415343221cd165f76ca5
John Ma <mayilinj@gmail.com>
2020-06-29 17:14:22 -0500

    updated package name

--- plugin/PlayAssetDelivery/src/androidTest/java/com/google/play/core/godot/ExampleInstrumentedTest.java
+++ plugin/PlayAssetDelivery/src/androidTest/java/com/google/play/core/godot/ExampleInstrumentedTest.java
@@ -14,20 +14,29 @@
   	limitations under the License.
  */
 
-package com.google.play.godot;
+package com.google.play.core.godot;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
+ * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.google.play.godotassetdelivery.test", appContext.getPackageName());
     }
 }
--- plugin/PlayAssetDelivery/src/main/java/com/google/play/core/godot/assetpacks/PlayAssetDelivery.java
+++ plugin/PlayAssetDelivery/src/main/java/com/google/play/core/godot/assetpacks/PlayAssetDelivery.java
@@ -14,7 +14,7 @@
   	limitations under the License.
  */
 
-package com.google.play.godot.assetpacks;
+package com.google.play.core.godot.assetpacks;
 
 import androidx.annotation.NonNull;
 
