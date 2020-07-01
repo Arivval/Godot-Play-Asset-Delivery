d90b1392dd87d3455811aae4abc600fc2a362d98
John Ma <mayilinj@gmail.com>
2020-07-01 14:09:07 -0500

    updated utils function logging setup

--- plugin/PlayAssetDelivery/src/main/java/com/google/play/core/godot/assetpacks/utils/PlayAssetDeliveryUtils.java
+++ plugin/PlayAssetDelivery/src/main/java/com/google/play/core/godot/assetpacks/utils/PlayAssetDeliveryUtils.java
@@ -21,6 +21,12 @@ import android.util.Log;
 import org.godotengine.godot.Dictionary;
 
 import com.google.android.play.core.assetpacks.AssetPackState;
+import com.google.android.play.core.assetpacks.AssetPackStates;
+import com.google.android.play.core.assetpacks.AssetLocation;
+import com.google.android.play.core.assetpacks.AssetPackLocation;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * This class contains all the helper methods for serialize/deserialize custom objects used in the
@@ -43,6 +49,71 @@ public class PlayAssetDeliveryUtils {
       return returnDict;
     } catch (Exception e) {
       Log.w(TAG, "Exception while converting AssetPackState object to Dictionary!", e);
+      return null;
+    }
+  }
+
+  public static Dictionary convertAssetPackStatesToDictionary(AssetPackStates assetPackStates) {
+    try {
+      Dictionary returnDict = new Dictionary();
+      returnDict.put("totalBytes", assetPackStates.totalBytes());
+
+      Map<String, AssetPackState> packStates = assetPackStates.packStates();
+      Dictionary packStatesDictionary = new Dictionary();
+
+      for (Map.Entry<String, AssetPackState> entry : packStates.entrySet()) {
+        String packName = entry.getKey();
+        AssetPackState packState = entry.getValue();
+        packStatesDictionary.put(packName, packState);
+      }
+
+      returnDict.put("packStates", packStatesDictionary);
+      return returnDict;
+    } catch (Exception e) {
+      Log.w(TAG, "Exception while converting AssetPackStates object to Dictionary!", e);
+      return null;
+    }
+  }
+
+  public static Dictionary convertAssetLocationToDictionary(AssetLocation assetLocation) {
+    try {
+      Dictionary returnDict = new Dictionary();
+      returnDict.put("offset", assetLocation.offset());
+      returnDict.put("path", assetLocation.path());
+      returnDict.put("size", assetLocation.size());
+      return returnDict;
+    } catch (Exception e) {
+      Log.w(TAG, "Exception while converting AssetLocation object to Dictionary!", e);
+      return null;
+    }
+  }
+
+  public static Dictionary convertAssetPackLocationToDictionary(
+      AssetPackLocation assetPackLocation) {
+    try {
+      Dictionary returnDict = new Dictionary();
+      returnDict.put("assetsPath", assetPackLocation.assetsPath());
+      returnDict.put("packStorageMethod", assetPackLocation.packStorageMethod());
+      returnDict.put("path", assetPackLocation.packStorageMethod());
+      return returnDict;
+    } catch (Exception e) {
+      Log.w(TAG, "Exception while converting AssetPackLocation object to Dictionary!", e);
+      return null;
+    }
+  }
+
+  public static Dictionary convertAssetPackLocationsToDictionary(
+      Map<String, AssetPackLocation> assetPackLocations) {
+    try {
+      Dictionary returnDict = new Dictionary();
+      for (Map.Entry<String, AssetPackLocation> entry : assetPackLocations.entrySet()) {
+        String packName = entry.getKey();
+        AssetPackLocation packLocation = entry.getValue();
+        returnDict.put(packName, packLocation);
+      }
+      return returnDict;
+    } catch (Exception e) {
+      Log.w(TAG, "Exception while converting AssetPackLocations to Dictionary!", e);
       return null;
     }
   }
@@ -84,6 +155,101 @@ public class PlayAssetDeliveryUtils {
       return packState;
     } catch (Exception e) {
       Log.w(TAG, "Exception while converting Dictionary to AssetPackState object!", e);
+      return null;
+    }
+  }
+
+  public static AssetPackStates convertDictionaryToAssetPackStates(final Dictionary dict) {
+    try {
+      AssetPackStates assetPackStates =
+          new AssetPackStates() {
+            @Override
+            public long totalBytes() {
+              return (long) dict.get("totalBytes");
+            }
+
+            @Override
+            public Map<String, AssetPackState> packStates() {
+              Map<String, AssetPackState> returnMap = new HashMap<String, AssetPackState>();
+              for (Map.Entry<String, Object> entry : dict.entrySet()) {
+                AssetPackState currentPackState =
+                    convertDictionaryToAssetPackState((Dictionary) entry.getValue());
+                returnMap.put(entry.getKey(), currentPackState);
+              }
+              return returnMap;
+            }
+          };
+      return assetPackStates;
+    } catch (Exception e) {
+      Log.w(TAG, "Exception while converting Dictionary to AssetPackStates object!", e);
+      return null;
+    }
+  }
+
+  public static AssetLocation convertDictionaryToAssetLocation(final Dictionary dict) {
+    try {
+      AssetLocation assetLocation =
+          new AssetLocation() {
+            @Override
+            public String path() {
+              return (String) dict.get("path");
+            }
+
+            @Override
+            public long offset() {
+              return (long) dict.get("offset");
+            }
+
+            @Override
+            public long size() {
+              return (long) dict.get("size");
+            }
+          };
+      return assetLocation;
+    } catch (Exception e) {
+      Log.w(TAG, "Exception while converting Dictionary to AssetLocation object!", e);
+      return null;
+    }
+  }
+
+  public static AssetPackLocation convertDictionaryToAssetPackLocation(final Dictionary dict) {
+    try {
+      AssetPackLocation assetPackLocation =
+          new AssetPackLocation() {
+            @Override
+            public int packStorageMethod() {
+              return (int) dict.get("packStorageMethod");
+            }
+
+            @Override
+            public String path() {
+              return (String) dict.get("path");
+            }
+
+            @Override
+            public String assetsPath() {
+              return (String) dict.get("assetsPath");
+            }
+          };
+      return assetPackLocation;
+    } catch (Exception e) {
+      Log.w(TAG, "Exception while converting Dictionary to AssetPackLocation object!", e);
+      return null;
+    }
+  }
+
+  public static Map<String, AssetPackLocation> convertDictionaryToAssetPackLocations(
+      final Dictionary dict) {
+    try {
+      Map<String, AssetPackLocation> returnMap = new HashMap<String, AssetPackLocation>();
+      for (Map.Entry<String, Object> entry : dict.entrySet()) {
+        AssetPackLocation currentPackLocation =
+            convertDictionaryToAssetPackLocation((Dictionary) entry.getValue());
+        returnMap.put(entry.getKey(), currentPackLocation);
+      }
+      return returnMap;
+    } catch (Exception e) {
+      Log.w(TAG, "Exception while converting Dictionary to AssetPackLocations!", e);
       return null;
     }
   }
