41ddc2ee7b37b0496ca62ef1b31bc788e1ba8c17
John Ma <mayilinj@gmail.com>
2020-07-17 17:03:43 -0500

    fixed a bug where const is set to something that should be mutable

--- api/test/test_helper/fake_android_plugin.gd
+++ api/test/test_helper/fake_android_plugin.gd
@@ -38,7 +38,7 @@ func _init():
 	_asset_location_store = Dictionary()
 	_asset_pack_location_store = Dictionary()
 	
-	_asset_pack_states_store = EMPTY_ASSET_PACK_STATES
+	_asset_pack_states_store = EMPTY_ASSET_PACK_STATES.duplicate()
 
 # -----------------------------------------------------------------------------
 # Utility Functions
@@ -68,7 +68,7 @@ func set_asset_pack_states_store(asset_pack_states_dict : Dictionary):
 	_asset_pack_states_store = asset_pack_states_dict
 
 func clear_asset_pack_states_store():
-	_asset_pack_states_store = EMPTY_ASSET_PACK_STATES
+	_asset_pack_states_store = EMPTY_ASSET_PACK_STATES.duplicate()
 
 func add_asset_pack_state(asset_pack_state_dict : Dictionary):
 	_asset_pack_states_store[PlayAssetPackStates._TOTAL_BYTES_KEY] += \
@@ -107,7 +107,7 @@ func getPackLocations():
 # updated states.
 # -----------------------------------------------------------------------------
 func cancel(pack_names : Array):
-	var return_asset_pack_states = EMPTY_ASSET_PACK_STATES
+	var return_asset_pack_states = EMPTY_ASSET_PACK_STATES.duplicate()
 
 	# iterate through all pack_names, if they exist in _asset_pack_states_store, try cancel them
 	for pack_name in pack_names:
