bb2dec1055e327815b7887e80e4f4c73b13b2e73
John Ma <mayilinj@gmail.com>
2020-07-21 17:25:11 -0500

    updated naming again

--- api/test/test_play_asset_pack_manager.gd
+++ api/test/test_play_asset_pack_manager.gd
@@ -173,7 +173,7 @@ func test_show_cellular_data_confirmation_success():
 	var request_object = test_object.show_cellular_data_confirmation()
 	
 	# connect to helper function, simulating the workflow of connecting callback to signal
-	request_object.connect("request_completed", self, "assert_cellular_data_confirmation_succeeded")
+	request_object.connect("request_completed", self, "assert_cellular_data_confirmation_is_success")
 	
 	# yield to the request_completed signal for no longer than 1 seconds and assert for signal emitted
 	yield(yield_to(request_object, "request_completed", 1), YIELD)
@@ -186,7 +186,7 @@ func test_show_cellular_data_confirmation_success():
 	# join instantiated thread
 	mock_plugin._show_confirmation_thread.wait_to_finish()
 
-func assert_cellular_data_confirmation_succeeded(status : bool, result : int, exception : PlayAssetPackException):
+func assert_cellular_data_confirmation_is_success(status : bool, result : int, exception : PlayAssetPackException):
 	# assert using callback, simulating the workflow of connecting callback to signal
 	assert_true(status)
 	assert_eq(result, PlayAssetPackManager.AssetPackStorageMethod.STORAGE_FILES)
@@ -232,7 +232,7 @@ func test_remove_pack_success():
 	var request_object = test_object.remove_pack("packName")
 	
 	# connect to helper function, simulating the workflow of connecting callback to signal
-	request_object.connect("request_completed", self, "assert_remove_pack_succeeded")
+	request_object.connect("request_completed", self, "assert_remove_pack_is_success")
 	
 	# yield to the request_completed signal for no longer than 1 seconds and assert for signal emitted
 	yield(yield_to(request_object, "request_completed", 1), YIELD)
@@ -244,7 +244,7 @@ func test_remove_pack_success():
 	# join instantiated thread
 	mock_plugin._remove_pack_thread.wait_to_finish()
 
-func assert_remove_pack_succeeded(status : bool, exception : PlayAssetPackException):
+func assert_remove_pack_is_success(status : bool, exception : PlayAssetPackException):
 	# assert using callback, simulating the workflow of connecting callback to signal
 	assert_true(status)
 	assert_eq(exception, null)
