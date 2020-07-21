60185c2be8573f0bc12b55dbba8c9e58a7d7e87d
John Ma <mayilinj@gmail.com>
2020-07-21 15:48:33 -0500

    fixed grammar edge case

--- api/test/test_helper/base_test_class.gd
+++ api/test/test_helper/base_test_class.gd
@@ -131,6 +131,6 @@ func create_mock_asset_pack_state_with_status_dict(pack_name : String, status : 
 func create_mock_asset_pack_java_lang_exception_dict():
 	return {
 		PlayAssetPackException._TYPE_KEY: "java.lang.Exception", 
-		PlayAssetPackException._MESSAGE_KEY: "Just an usual exception", 
+		PlayAssetPackException._MESSAGE_KEY: "Just a usual exception", 
 		PlayAssetPackException._ERROR_CODE_KEY: -100
 	}
